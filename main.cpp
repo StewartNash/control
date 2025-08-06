1669759a76e65feaebf98a68dc5515499bbc3596
pervigilent <pervigilent@gmail.com>
2025-08-05 22:38:51 -0700

    Restarted main application, moved example

--- main.cpp
+++ main.cpp
@@ -1,67 +1,41 @@
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-struct SDLState {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-};
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+static SDL_FPoint points[500];
 
-void cleanup(SDLState &state);
-
-int main(int argc, char* argv[]) {
-	SDLState state;
-
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+	int i;
+  
+	SDL_SetAppMetadata("Example Renderer Primitives", "1.0", "com.example.renderer-primitives");
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
-
-		return 1;
+		SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+	if (!SDL_CreateWindowAndRenderer("examples/renderer/primitives", 640, 480, 0, &window, &renderer)) {
+		SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
 	}
 
-	// Create window
-	int width = 720;
-	int height = 480;
-	state.window = SDL_CreateWindow("SDL3 Demo", width, height, 0);		
-	if (!state.window) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", state.window);
-		cleanup(state);
-
-		return 1;
-	}
-
-	// Create renderer
-	state.renderer = SDL_CreateRenderer(state.window, nullptr);
-	if (!state.renderer) {
-		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating renderer", state.window);
-		cleanup(state);
-
-		return 1;
-	}
-
-	// Start game loop
-	bool running = true;
-	while (running) {
-		SDL_Event event{ 0 };
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-				case SDL_EVENT_QUIT:
-					running = false;
-					break;
-			}
-		}
-		// Draw
-		SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
-		SDL_RenderClear(state.renderer);
-
-		SDL_RenderPresent(state.renderer);
-	}
-
-	cleanup(state);
-
-	return 0;
+	return SDL_APP_CONTINUE;
 }
 
-void cleanup(SDLState &state) {
-	SDL_DestroyRenderer(state.renderer);
-	SDL_DestroyWindow(state.window);
-	SDL_Quit();
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+	if (event->type == SDL_EVENT_QUIT) {
+		return SDL_APP_SUCCESS;
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+	SDL_FRect rect;
+
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+
 }
