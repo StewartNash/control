8fdbd531e878dbaffc993634faaf9aa0ee6d0982
StewartNash <Stewart.M.Nash@gmail.com>
2025-08-10 04:27:12 -0700

    Started to add custom classes.

---
+++ models.hpp
@@ -0,0 +1,40 @@
+#include <vector>
+
+// Signal Flow Graphs
+
+class Node {
+	public:
+		std::vector<Branch> inputs;
+		std::vector<Branch> outputs;
+
+};
+
+class Branch {
+
+};
+
+class SignalFlowGraph {
+
+};
+
+// Block Diagrams
+
+class Block {
+
+};
+
+class Signal {
+
+};
+
+class SummingJunction {
+
+};
+
+class TakeoffPoint {
+
+};
+
+class BlockDiagram {
+
+};
